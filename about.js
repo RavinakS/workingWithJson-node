2d582865f97dd7827db2c894ae34096eee7520ec
RavinakS <ravinasingh697@gmail.com>
2021-08-09 11:58:49 +0530

    just doing some implementation to understand json with node.js

---
+++ about.js
@@ -0,0 +1,12 @@
+const information = {};
+information["bharti"] = {
+    "name": "bharti",
+    "address": "electronic city, bangalore",
+    "phone": 2345678123
+};
+
+information["zeba"] = {
+    "name": "zeba",
+    "address": "koramangala, bangalore",
+    "phone": 1234567890
+}
