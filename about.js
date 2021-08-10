01ddc0f3e2bf963f81d67433b11c290474b7534a
RavinakS <ravinasingh697@gmail.com>
2021-08-10 11:58:17 +0530

    reading json file with node.js

--- about.js
+++ about.js
@@ -1,3 +1,16 @@
+const fs = require('fs');
+fs.readFile('data.json', 'utf-8', (err, jsonData)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(jsonData);
+        const objectData = JSON.parse(jsonData);
+        console.log(objectData.bharti.address);
+    }
+})
+
+
+
 // const information = {};
 // information["bharti"] = {
 //     "name": "bharti",
@@ -11,5 +24,5 @@
 //     "phone": 1234567890
 // }
 
-const data = require('./data.json');
-console.log(data["bharti"]["address"]);
+// const data = require('./data.json'); // quick access
+// console.log(typeof data);
