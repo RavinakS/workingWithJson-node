132059a8238d540527ba62050f3d28684899273a
RavinakS <ravinasingh697@gmail.com>
2021-08-10 16:08:51 +0530

    fs.readFileSync()

--- about.js
+++ about.js
@@ -1,17 +1,4 @@
 const fs = require('fs');
-fs.readFile('data.json', 'utf-8', (err, jsonData)=>{
-    if(err){
-        console.log(err);
-    }else{
-        // console.log(typeof jsonData); // string
-        const objectData = JSON.parse(jsonData); // JSON.parse converts the json string into object
-        // console.log(typeof objectData); // object
-        // console.log(jsonData.bharti.address); // undefined error because a string doesn't have (property)key:value
-        console.log(objectData.bharti.address);
-    }
-})
-
-
 
 // const information = {};
 // information["bharti"] = {
@@ -26,5 +13,32 @@ fs.readFile('data.json', 'utf-8', (err, jsonData)=>{
 //     "phone": 1234567890
 // }
 
-// const data = require('./data.json'); // quick access
+
+
+// fs.readFileSync()- takes two parameter 1. path(file path wants to read) 2. option(optional)
+
+const data = fs.readFileSync('data.json');
+const objecData = JSON.parse(data);
+console.log(objecData);
+
+
+
+// fs.readFile() - readFile() takes three arguments fileName, encoder, and a callback function which takes two parameters err and data
+// (err, data)={} 
+
+// fs.readFile('data.json', 'utf-8', (err, jsonData)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         // console.log(typeof jsonData); // string
+//         const objectData = JSON.parse(jsonData); // JSON.parse converts the json string into object
+//         // console.log(typeof objectData); // object
+//         // console.log(jsonData.bharti.address); // undefined error because a string doesn't have (property)key:value
+//         console.log(objectData.bharti.address);
+//     }
+// });
+
+
+// quick access
+// const data = require('./data.json'); 
 // console.log(typeof data);
