d81b6a1d008da105028ebe405b16a18db2269811
RavinakS <ravinasingh697@gmail.com>
2021-08-11 15:21:14 +0530

    fs.writeFile()

--- about.js
+++ about.js
@@ -1,25 +1,37 @@
 const fs = require('fs');
 
-// const information = {};
-// information["bharti"] = {
-//     "name": "bharti",
-//     "address": "electronic city, bangalore",
-//     "phone": 2345678123
-// };
+const information = {};
+information["bharti"] = {
+    "name": "bharti",
+    "address": "electronic city, bangalore",
+    "phone": 2345678123
+};
 
-// information["zeba"] = {
-//     "name": "zeba",
-//     "address": "koramangala, bangalore",
-//     "phone": 1234567890
-// }
+information["zeba"] = {
+    "name": "zeba",
+    "address": "koramangala, bangalore",
+    "phone": 1234567890
+}
+
+const jsonString = JSON.stringify(information, null, 2);
+// to write data to a JSON file, we must create a JSON string of the data with JSON.stringify()
+
+fs.writeFile("data.json", jsonString, "utf-8", (err, data)=>{
+    if(err){
+        console.log("Couldn't write", err);
+    }else{
+        console.log("Successfully wrote file.");
+    }
+})
+console.log(jsonString);
 
 
 
 // fs.readFileSync()- takes two parameter 1. path(file path wants to read) 2. option(optional)
 
-const data = fs.readFileSync('data.json');
-const objecData = JSON.parse(data);
-console.log(objecData);
+// const data = fs.readFileSync('data.json');
+// const objecData = JSON.parse(data);
+// console.log(objecData);
 
 
 
